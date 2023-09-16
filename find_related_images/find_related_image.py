c034b193cc69f25cedb31127f4e0070f2f31ed54
Victor Huang <vichua2006@gmail.com>
2023-09-16 15:15:41 -0300

    added templates and static folder

--- find_related_images/find_related_image.py
+++ find_related_images/find_related_image.py
@@ -1,9 +1,9 @@
 from serpapi import GoogleSearch 
-import json
+import os 
 import requests
 
 # api key for: https://serpapi.com/
-API_KEY = "9d7067c59ad8799a799ddada89d845948a87c6391e08799854285cc6c2458726"
+API_KEY = os.getenv("SERPAPI_KEY")
 
 params = {
   "engine": "google_images",
@@ -23,3 +23,5 @@ with open(r".\find_related_images\images\test_image.jpg", "wb") as file:
     file.write(img_data)
 
 print(images[0]["original"])
+
+# potentially implement a image-caption similarity checker
