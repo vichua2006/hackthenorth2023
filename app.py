e4da6c5045ba6d61fdb18226cd3a7c1f6caee13d
Victor Huang <vichua2006@gmail.com>
2023-09-16 16:10:42 -0300

    basic flask site

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="templates", static_folder="statics")
 
 @app.route('/')
+@app.route('/index')
+@app.route('/presentation', methods=['POST'])
+
 def index():
-    return "Enter a prompt"
+    return render_template('index.html')
+
+def presentation():
+    return render_template('presentation.html', script=request.args.get('script'))
+    
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
