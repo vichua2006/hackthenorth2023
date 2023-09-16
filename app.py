57146e0474074cbe114872f987e9234a446aaff3
Ching Lam Lau <contact.minicube11@gmail.com>
2023-09-16 15:29:32 -0400

    Submit form + create slideshow page

--- app.py
+++ app.py
@@ -5,3 +5,16 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template("index.html")
+
+@app.route('/slideshow', methods=['POST'])
+def slideshow():
+    slides = [
+        {
+            "title": "Hi!",
+            "points": [
+                "point 1",
+                "point 2",
+            ]
+        }
+    ]
+    return render_template("slideshow.html", slideshow=slides)
